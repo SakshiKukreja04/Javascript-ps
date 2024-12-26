6601225907f585bb65b82294be690f4a5b962824
SakshiKukreja04 <164363445+SakshiKukreja04@users.noreply.github.com>
2024-12-26 17:48:29 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// let score = null;
+// let mark = undefined;
+
+// let ValueInNumber = Number(score)
+// let ValueInMark = Number(mark)
+// console.log(ValueInNumber);
+// console.log(ValueInMark)
+
+
+
+
+// conversion
+// 33=> 33
+// "33abc" => NaN
+// true => 1; false=>0
+// null=>0
+// undefined=> NaN
+
+// let isloggedIn = 1
+// let Name = "Sakshi"
+// let ValueInLogged = Boolean(isloggedIn)
+// let ValueInName = Boolean(Name)
+// console.log(ValueInLogged);
+// console.log(ValueInName);
+
+// 1=>true; 0=>false
+// "" =>true
+// "Sakshi"= true
+
+
+let someValue = 66
+
+let Valueinsome = String(someValue)
+console.log(Valueinsome)
+console.log(typeof Valueinsome)
+
+
