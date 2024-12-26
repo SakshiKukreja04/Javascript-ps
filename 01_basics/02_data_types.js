b367713249a3550071cb3d829b0056383c6bd32d
SakshiKukreja04 <164363445+SakshiKukreja04@users.noreply.github.com>
2024-12-26 17:07:28 +0000

    datatypes

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,24 @@
+"use strict"
+
+// alert(3+3) we are in nodejs not browser
+
+// always ensure code readability
+
+
+let age=18
+let Name = "Sakshi"
+let state
+
+// number => ranges from 2 power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => uniqueness
+
+console.log(typeof "sakshi");
+
+console.log(typeof undefined) // output>undefined 
+console.log(typeof null); // output>object
+
