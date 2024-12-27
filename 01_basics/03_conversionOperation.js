b5f4999e87dfc8d40a540d983e341fdfe99d998b
SakshiKukreja04 <164363445+SakshiKukreja04@users.noreply.github.com>
2024-12-27 14:14:44 +0000

    conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -34,4 +34,36 @@ let Valueinsome = String(someValue)
 console.log(Valueinsome)
 console.log(typeof Valueinsome)
 
+// ----------Operations---------
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(1/2);
+// console.log(1%2);
+
+let str1 = "sakshi"
+let str2 = " hello"
+
+let str3 = str1+str2
+console.log(str3);
+
+
+
+// console.log(2 + "2")
+// console.log("2" + 1 + 1)
+// console.log(1 +1 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+// studying of prefix and postfix from mdn
+
+
+
 
