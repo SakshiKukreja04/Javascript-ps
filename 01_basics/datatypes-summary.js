f9f6b63342ed8437507da37c606c2c4918e5053b
SakshiKukreja04 <164363445+SakshiKukreja04@users.noreply.github.com>
2024-12-27 15:11:33 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,28 @@
+// JavaScript is dynamically typed: 
+// Explanation
+// In a dynamically typed language, the interpreter assigns a type to a variable based on the value of the variable at runtime. This means that you can assign any type of value to a variable without explicitly declaring the type.
+let number = 100
+console.log(typeof number);
+
+
+// Primitive 7 types
+// Number, String, Boolean,null,undefined,symbol,BigInt
+
+// Non-Primitive
+// Array,Objects,Functions
+
+const hero = ["ironman","spiderman","doga"];
+let myObject = {
+    name: "sakshi",
+    score:20
+}
+const myFunction = function(){
+    console.log("sakshi");
+    
+}  
+myFunction();
+
+
+// for read ecma scripts
+
+// for knowing types we use typeof to know
