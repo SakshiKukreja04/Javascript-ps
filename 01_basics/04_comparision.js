0e2dd96455a14cb49f95831aa079af827376d49a
SakshiKukreja04 <164363445+SakshiKukreja04@users.noreply.github.com>
2024-12-27 14:26:00 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2>=1);
+
+console.log("2"+1) 
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined>=0);
+// always gives false
+
+console.log(2 == "2"); // here it will convert datatype,hence true
+console.log(2 === "2");// here it will not convert datatype ie strict check for datatype too,hence false
