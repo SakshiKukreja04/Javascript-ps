6bbb98d008c25458cd8fc8a205c7a8fc467fe04b
SakshiKukreja04 <164363445+SakshiKukreja04@users.noreply.github.com>
2025-01-10 18:02:09 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,43 @@
+//if
+
+if(2=="2"){
+    console.log("yes");
+    
+}
+else{
+    console.log("No");
+    
+}
+
+// = is a assignment operator
+// == checks the statement
+// === checks as well as typeof
+
+// >,<,<=,>=,==,===,!=,!==
+
+
+const score = 100
+
+if(score>200){
+    const power = "fly"
+    console.log(`The power:${power}`);
+    
+}
+// console.log(`The power:${power}`); undefined
+
+const balance = 1000
+
+// if(balance>100) console.log("budget acha h");
+
+const userLoggedIn = true
+const userDebit = true
+const googleLoggedIn = false
+
+if(userDebit && userLoggedIn){
+    console.log("You are logged In")
+}
+if(userLoggedIn || googleLoggedIn){
+    console.log("you are logged in");
+}
+
+
