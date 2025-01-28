99e35205626df7688258182cf53bd3c531c5d244
SakshiKukreja04 <164363445+SakshiKukreja04@users.noreply.github.com>
2025-01-28 19:21:13 +0000

    loop

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,6 +1,39 @@
 const lang = ["ruby","cpp","java","js"]
 
-lang.forEach((val) => {
-    console.log(lang);
-    
-});
+// lang.forEach(function (val){
+//     console.log(val)
+// })
+
+// lang.forEach((val)=>{
+//     console.log(val)
+// })
+
+// function printme(item){
+//     console.log(item)
+// }
+
+// lang.forEach(printme)
+
+lang.forEach((item,index,arr)=>{
+    console.log(item,index,arr)
+})
+
+
+const myArr = [
+    {
+        language:'javascript',
+        language_short : 'js'
+    },
+    {
+        language:'javascript',
+        language_short : 'js'
+    },
+    {
+        language:'javascript',
+        language_short : 'js'
+    }
+]
+
+myArr.forEach((item)=>{
+    console.log(item.language)
+})
